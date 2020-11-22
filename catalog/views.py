e93f3b115646abe5890f578e7b3703b8ccbf0f3e
RamKotto <black8.tabls@gmail.com>
2020-11-22 22:40:06 +0300

    forms for books and authors

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .models import Genre, Language, Book, BookInstance, Author
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 import datetime
 from .forms import RenewBookForm
@@ -124,3 +125,40 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html',
                   {'form' : form, 'bookinst' : book_inst})
+
+
+# Ниже предоставлены 3 вьюшки, с помощью которых можно избежать отдельного создания форм
+# Create and Update будут искать шаблон model_name_form.html
+# Delete будет искать model_name_confirm_delete.html
+# Это можно переопределить с помощью поля 'template_name_suffix'
+class AuthorCreate(CreateView):
+    model = Author   # Модель, поля которой нам нужны (что мы и будем создавать)
+    fields = '__all__'   # Добавляем все поля модели
+    initial = {'date_of_birth' : '18.11.1990',}  # Данные, которые будут предложены
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name',
+              'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')   # Редирект, после удаления автора
+
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+
+# Аналогично Author
+class BookUpdate(UpdateView):
+    model = Book
+    fields = '__all__'
+
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
